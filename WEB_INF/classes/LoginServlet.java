baaceae9cf18be0200a0a833cfbbf6cefa0e16da
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2023-11-06 22:54:41 +0530

    ex6c

---
+++ WEB_INF/classes/LoginServlet.java
@@ -0,0 +1,10 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+        dispatcher.forward(request, response);
+    }
+}
