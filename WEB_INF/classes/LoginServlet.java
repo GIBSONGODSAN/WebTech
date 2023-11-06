2f2128006e18e00bec4e5e626ff963b9cb06846f
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2023-11-06 23:12:31 +0530

    ex6d

--- WEB_INF/classes/LoginServlet.java
+++ WEB_INF/classes/LoginServlet.java
@@ -1,10 +1,25 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
-        dispatcher.forward(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Verify the username and password (for simplicity, always assume valid for this example)
+        if (true) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("jsp/WelcomePage.jsp");
+        } else {
+            response.sendRedirect("jsp/ErrorPage.jsp");
+        }
     }
 }
